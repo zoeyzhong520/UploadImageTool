c16b1978bd3d17fb7fceef737d4803612bf4131e
AsiaZhang <17612156438@sina.cn>
2018-06-05 19:21:30 +0800

    1添加保存图片到相册

--- UploadImageTool/UploadImageTool/UploadImageTool/UploadImageTool.m
+++ UploadImageTool/UploadImageTool/UploadImageTool/UploadImageTool.m
@@ -60,27 +60,33 @@ static UploadImageTool *uploadImageTool = nil;
     imagePC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     imagePC.delegate = self;
     imagePC.allowsEditing = YES;
-    /*
-     [fatherViewController presentViewController:imagePC animated:YES completion:^{
-     }];
-     */
+    [fatherViewController presentViewController:imagePC animated:YES completion:^{
+        
+    }];
     
-    UIPopoverController *popover = [[UIPopoverController alloc] initWithContentViewController:imagePC];
-    popover.popoverContentSize = CGSizeMake(600, 800);//弹出窗口大小，如果屏幕画不下，会挤小的。这个值默认是320x1100
-    CGRect popoverRect = CGRectMake(0, screenHeight-200, screenWidth, 200);
-    //popoverRect的中心点是用来画箭头的，如果中心点如果出了屏幕，系统会优化到窗口边缘
-    //上面的矩形坐标是以这个view为参考的
-    [popover presentPopoverFromRect:popoverRect inView:fatherViewController.view permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+    /*
+     UIPopoverController *popover = [[UIPopoverController alloc] initWithContentViewController:imagePC];
+     popover.popoverContentSize = CGSizeMake(600, 800);//弹出窗口大小，如果屏幕画不下，会挤小的。这个值默认是320x1100
+     CGRect popoverRect = CGRectMake(0, screenHeight-200, screenWidth, 200);
+     //popoverRect的中心点是用来画箭头的，如果中心点如果出了屏幕，系统会优化到窗口边缘
+     //上面的矩形坐标是以这个view为参考的
+     [popover presentPopoverFromRect:popoverRect inView:fatherViewController.view permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+     */
 }
 
 #pragma mark -- UIImagePickerControllerDelegate
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
     NSLog(@"%@",info);
+    
     [picker dismissViewControllerAnimated:YES completion:^{
         
     }];
     
     UIImage *portraitImg = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    [self saveImageToIphone:portraitImg];
+    
     //png -> jpg
     NSData *data;
     if (UIImagePNGRepresentation(portraitImg) == nil) {
@@ -89,27 +95,29 @@ static UploadImageTool *uploadImageTool = nil;
         data = UIImagePNGRepresentation(portraitImg);
     }
     float length = [data length]/1024;
-    NSLog(@"%f",length);
+    NSLog(@"图片压缩前的大小：%f",length);
     
-    /*
-     //先判断宽高哪个短用哪个来做截取标准
-     CGImageRef cgRef = portraitImg.CGImage;
-     if (portraitImg.size.width > portraitImg.size.height) {
-     CGImageRef imageRef = CGImageCreateWithImageInRect(cgRef, CGRectMake((portraitImg.size.width-portraitImg.size.height)/2, 0, portraitImg.size.width, portraitImg.size.width));
-     UIImage *thumbScale = [UIImage imageWithCGImage:imageRef];
-     portraitImg = thumbScale;
-     }else{
-     CGImageRef imageRef = CGImageCreateWithImageInRect(cgRef, CGRectMake(0, (portraitImg.size.height-portraitImg.size.width)/2, portraitImg.size.height, portraitImg.size.height));
-     UIImage *thumbScale = [UIImage imageWithCGImage:imageRef];
-     portraitImg = thumbScale;
-     }
-     [self scaleToSize:portraitImg size:CGSizeMake(portraitImg.size.width*300/portraitImg.size.height, 300)];
-     */
+    //先判断宽高哪个短用哪个来做截取标准
+    CGImageRef cgRef = portraitImg.CGImage;
+    if (portraitImg.size.width > portraitImg.size.height) {
+        CGImageRef imageRef = CGImageCreateWithImageInRect(cgRef, CGRectMake((portraitImg.size.width-portraitImg.size.height)/2, 0, portraitImg.size.width, portraitImg.size.width));
+        UIImage *thumbScale = [UIImage imageWithCGImage:imageRef];
+        portraitImg = thumbScale;
+    }else{
+        CGImageRef imageRef = CGImageCreateWithImageInRect(cgRef, CGRectMake(0, (portraitImg.size.height-portraitImg.size.width)/2, portraitImg.size.height, portraitImg.size.height));
+        UIImage *thumbScale = [UIImage imageWithCGImage:imageRef];
+        portraitImg = thumbScale;
+    }
     
+    UIImage *scaledImage = [self scaleToSize:portraitImg size:CGSizeMake(portraitImg.size.width*300/portraitImg.size.height, 300)];
+    
+    NSData *scaledImageData = UIImagePNGRepresentation(scaledImage);
+    float scaledLenth = [scaledImageData length] / 1024;
+    NSLog(@"图片压缩后的大小：%f", scaledLenth);
     
     //上传图片
     if (self.uploadImageDelegate && [self.uploadImageDelegate respondsToSelector:@selector(uploadImageToServerWithImage:)]) {
-        [self.uploadImageDelegate uploadImageToServerWithImage:portraitImg];
+        [self.uploadImageDelegate uploadImageToServerWithImage:scaledImage];
     }
 }
 
@@ -127,5 +135,33 @@ static UploadImageTool *uploadImageTool = nil;
     //返回新的改变大小后的图片
     return scaledImage;
 }
+
+///保存图片到相册
+- (void)saveImageToIphone:(UIImage *)image {
+    
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+}
+
+//回调方法
+- (void)image: (UIImage *) image didFinishSavingWithError: (NSError *) error contextInfo: (void *) contextInfo
+{
+    NSString *msg = nil ;
+    if(error != NULL){
+        msg = @"保存图片失败" ;
+    }else{
+        msg = @"保存图片成功" ;
+    }
+    
+    NSLog(@"%@", msg);
+    
+    /*
+     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:msg preferredStyle:UIAlertControllerStyleAlert];
+     [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+     
+     }]];
+     [fatherViewController showViewController:alert sender:nil];
+     */
+}
+
 
 @end
